f8018ea015f2c4a439ae73cc60e437921734b84b
Felipe GARIBOTTI <fgaribot@e1r11p9.clusters.42paris.fr>
2022-09-28 17:13:43 +0200

    suite du tuto

--- app.js
+++ app.js
@@ -13,8 +13,12 @@
 // npm init (step by step, press enter to skip)
 // npm init -y (everithing default)
 
+// npm install - setup the node_modules folder
+// npm uninstall - uninstall a module
+
 const _ = require('lodash')
 
 const items = [1, [2, [3, [4]]]]
 const newItems  = _.flattenDeep(items)
 console.log(newItems)
+console.log('Hello People')
