f7c61607915587f5000fe8bbf924f8822bb768a9
Felipe GARIBOTTI <fgaribot@e1r11p9.clusters.42paris.fr>
2022-09-29 14:35:49 +0200

    clean folder

--- 4-names.js
+++
@@ -1,5 +0,0 @@
-const secret = "Super Secret"
-const john = "john"
-const petter = "petter"
-
-module.exports = {petter, john}
--- 6-alternative-flavors.js
+++
@@ -1,7 +0,0 @@
-const items = ['item1', 'item2']
-const person = {
-    name: 'bob',
-}
-
-
-module.exports = {items, person}
